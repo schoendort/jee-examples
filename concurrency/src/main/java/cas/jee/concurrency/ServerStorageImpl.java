f8d602fd97419cb8ce7cfc63afc5fd04755369c3
schoendort <tschoendorfer92@gmail.com>
2017-02-08 23:32:23 +0100

    add default annotations

--- concurrency/src/main/java/cas/jee/concurrency/ServerStorageImpl.java
+++ concurrency/src/main/java/cas/jee/concurrency/ServerStorageImpl.java
@@ -6,6 +6,10 @@ import java.util.Map;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
+import javax.ejb.ConcurrencyManagement;
+import javax.ejb.ConcurrencyManagementType;
+import javax.ejb.Lock;
+import javax.ejb.LockType;
 import javax.ejb.Remote;
 import javax.ejb.Singleton;
 
@@ -13,6 +17,7 @@ import cas.jee.ejb.client.ServerStorage;
 
 @Singleton
 @Remote(ServerStorage.class)
+@ConcurrencyManagement(ConcurrencyManagementType.CONTAINER)
 public class ServerStorageImpl implements ServerStorage {
 
 	private static final Logger LOGGER = Logger.getLogger(ServerStorageImpl.class.getName());
@@ -20,23 +25,27 @@ public class ServerStorageImpl implements ServerStorage {
 	private Map<String, String> storage = new HashMap<>();
 
 	@Override
+	@Lock(LockType.WRITE)
 	public void saveData(String key, String value) {
 		LOGGER.severe("save data [" + key + "=" + value + "]");
 		storage.put(key, value);
 	}
 
 	@Override
+	@Lock(LockType.WRITE)
 	public String readData(String key) {
 		LOGGER.severe("read data for key = [" + key + "]");
 		return storage.get(key);
 	}
 
 	@Override
+	@Lock(LockType.WRITE)
 	public List<String> filterValues(String contains) {
 		return storage.values().stream().filter(v -> v.contains(contains)).collect(Collectors.toList());
 	}
 
 	@Override
+	@Lock(LockType.WRITE)
 	public void removeData(String key) {
 		storage.remove(key);
 	}
