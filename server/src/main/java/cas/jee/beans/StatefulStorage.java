6e2063f9d4c8327a79a2990d0beb01c76359f88f
schoendort <tschoendorfer92@gmail.com>
2017-02-09 00:39:19 +0100

    show difference between stateless and stateful

---
+++ server/src/main/java/cas/jee/beans/StatefulStorage.java
@@ -0,0 +1,39 @@
+package cas.jee.beans;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.ejb.Remote;
+import javax.ejb.Stateful;
+
+import cas.jee.ejb.client.ServerStorage;
+
+@Stateful
+@Remote(ServerStorage.class)
+public class StatefulStorage implements ServerStorage {
+
+	private Map<String, String> storage = new HashMap<>();
+
+	@Override
+	public void saveData(String key, String value) {
+		storage.put(key, value);
+	}
+
+	@Override
+	public String readData(String key) {
+		return storage.get(key);
+	}
+
+	@Override
+	public List<String> filterValues(String contains) {
+		return storage.values().stream().filter(v -> v.contains(contains)).collect(Collectors.toList());
+	}
+
+	@Override
+	public void removeData(String key) {
+		storage.remove(key);
+	}
+
+}
