d177c4b79ecdd143bd082e9430c4d9d53f1da7c6
SchoendorT <SchoendorT@NB2181.corp.eisenmann.corp>
2017-02-10 13:29:06 +0100

    remove comments

--- client/src/main/java/cas/jee/client/concurrency/StateMain.java
+++ client/src/main/java/cas/jee/client/concurrency/StateMain.java
@@ -8,31 +8,42 @@ import javax.naming.NamingException;
 
 import cas.jee.ejb.client.ServerStorage;
 
-public class StateMain {
+public class StateMain
+{
 
-	public static void main(String[] args) throws InterruptedException {
-		final Properties env = new Properties();
-		env.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
-		env.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
-		env.put("jboss.naming.client.ejb.context", true);
-		try {
-			Context remoteContext = new InitialContext(env);
-			ServerStorage storageStateful = (ServerStorage) remoteContext
-					.lookup("jeeexample/StatefulStorage!cas.jee.ejb.client.ServerStorage");
-			ServerStorage storageStateless = (ServerStorage) remoteContext
-					.lookup("jeeexample/StatelessStorage!cas.jee.ejb.client.ServerStorage");
-			storageStateful.saveData("key", "value");
-			storageStateless.saveData("key", "value");
-			System.out.println(storageStateless.readData("key"));
-			System.out.println(storageStateful.readData("key"));
-			ServerStorage storageStateless2 = (ServerStorage) remoteContext
-					.lookup("jeeexample/StatelessStorage!cas.jee.ejb.client.ServerStorage");
-			ServerStorage storageStateful2 = (ServerStorage) remoteContext
-					.lookup("jeeexample/StatefulStorage!cas.jee.ejb.client.ServerStorage");
-			System.out.println(storageStateless2.readData("key")); //returns value but this is not guaranteed
-			System.out.println(storageStateful2.readData("key")); //returns null -> new session new stateful bean instance
-		} catch (NamingException e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args)
+        throws InterruptedException
+    {
+        final Properties env = new Properties();
+        env.put(Context.INITIAL_CONTEXT_FACTORY,
+                "org.jboss.naming.remote.client.InitialContextFactory");
+        env.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
+        env.put("jboss.naming.client.ejb.context", true);
+        try
+        {
+            Context remoteContext = new InitialContext(env);
+            ServerStorage storageStateful = (ServerStorage) remoteContext
+                .lookup("jeeexample/StatefulStorage!cas.jee.ejb.client.ServerStorage");
+            ServerStorage storageStateless = (ServerStorage) remoteContext
+                .lookup("jeeexample/StatelessStorage!cas.jee.ejb.client.ServerStorage");
+
+            storageStateful.saveData("key", "value");
+            storageStateless.saveData("key", "value");
+
+            System.out.println(storageStateless.readData("key"));
+            System.out.println(storageStateful.readData("key"));
+
+            ServerStorage storageStateless2 = (ServerStorage) remoteContext
+                .lookup("jeeexample/StatelessStorage!cas.jee.ejb.client.ServerStorage");
+            ServerStorage storageStateful2 = (ServerStorage) remoteContext
+                .lookup("jeeexample/StatefulStorage!cas.jee.ejb.client.ServerStorage");
+
+            System.out.println(storageStateless2.readData("key"));
+            System.out.println(storageStateful2.readData("key"));
+        }
+        catch ( NamingException e )
+        {
+            e.printStackTrace();
+        }
+    }
 }
